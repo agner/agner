42cdcbc99a5fd8a0421654f9a6a8555833e830de
Yurii Rashkovskii <yrashk@gmail.com>
2011-01-25 10:48:51 -0800

    Reverse parsed spec so that it looks like original

--- src/agner_github.erl
+++ src/agner_github.erl
@@ -67,17 +67,18 @@ spec(Name, SHA1) ->
 			{struct, Blob} = proplists:get_value(<<"blob">>, Object),
 			S = binary_to_list(proplists:get_value(<<"data">>, Blob)),
             {ok, Tokens, _ } = erl_scan:string(S),
-            lists:map(fun (Term) ->
-                              {ok, T} = erl_parse:parse_term(lists:reverse(Term)),
-                              T
-                      end,
-                      tl(lists:foldl(fun ({dot, _}=Dot,[Term|Terms]) ->
-                                          [[]|[[Dot|Term]|Terms]];
-                                         (Token, [Term|Terms]) ->
-                                             [[Token|Term]|Terms];
-                                         (Token, []) ->
-                                             [[Token]]
-                                     end, [], Tokens)))
+            lists:reverse(
+              lists:map(fun (Term) ->
+                                {ok, T} = erl_parse:parse_term(lists:reverse(Term)),
+                                T
+                        end,
+                        tl(lists:foldl(fun ({dot, _}=Dot,[Term|Terms]) ->
+                                               [[]|[[Dot|Term]|Terms]];
+                                           (Token, [Term|Terms]) ->
+                                               [[Token|Term]|Terms];
+                                           (Token, []) ->
+                                               [[Token]]
+                                       end, [], Tokens))))
 	end.
 
 
