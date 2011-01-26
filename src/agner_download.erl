114e3b32405e29cc46adc1fa34192b966485b977
Yurii Rashkovskii <yrashk@gmail.com>
2011-01-26 14:38:15 -0800

    When git clone exists, just refresh it.

--- src/agner_download.erl
+++ src/agner_download.erl
@@ -2,14 +2,22 @@
 -export([fetch/2]).
 
 fetch({git, URL, Ref}, Directory) ->
-    PortClone = git(["clone", URL, Directory]),
-    process_port(PortClone, fun () ->
-                                    PortCheckout = git(["checkout",Ref],[{cd, Directory}]),
-                                    process_port(PortCheckout, fun () -> ok end)
-                            end).
+    case filelib:is_dir(Directory) of
+        false -> %% clone
+            PortClone = git(["clone", URL, Directory]),
+            process_port(PortClone, fun () -> git_checkout(Ref, Directory) end);
+        true -> %% existing repo (or something else)
+            PortFetch = git(["fetch","origin"]),
+            process_port(PortFetch, fun() -> git_checkout(Ref, Directory) end)
+    end.
+                                       
 
 %%
 
+git_checkout(Ref, Directory) ->
+    PortCheckout = git(["checkout","-q","origin/" ++ Ref],[{cd, Directory}]),
+    process_port(PortCheckout, fun () -> ok end).
+    
 git(Args) ->
     git(Args,[]).
 
