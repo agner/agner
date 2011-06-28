0edbd79a992008589e34e8824a1c598f5e37ed90
Yurii Rashkovskii <yrashk@gmail.com>
2011-06-28 11:29:27 -0700

    Instead of restartign agner in rebar plugin, ensure agner is loaded.

--- src/agner_rebar_plugin.erl
+++ src/agner_rebar_plugin.erl
@@ -9,20 +9,27 @@
     pre_compile(Config, AppFile).
 
 'get-deps'(Config, AppFile) ->
-    agner:start(),
+    ensure_agner_started(),
     set_indices(Config),
     agner_main:handle_command(fetch,[{app, AppFile},{version, "@master"},{addpath, false},
-                                     {install, false},{build, false}]),
-    agner:stop().
+                                     {install, false},{build, false}]).
 
 pre_compile(Config, AppFile) ->
-    agner:start(),
+    ensure_agner_started(),
     set_indices(Config),
     agner_main:handle_command(build,[{app, AppFile},{version, "@master"},{quiet, false},
                                      {nofetch, true},
                                      {addpath, false},
-                                     {install, false}]),
-    agner:stop().
+                                     {install, false}]).
 
 set_indices(Config) ->
         application:set_env(agner, indices, rebar_config:get_local(Config, agner_indices, [{github,"agner"}])).
+
+ensure_agner_started() ->
+    case proplists:get_value(agner, application:loaded_applications()) of
+        undefined ->
+            agner:start();
+        _ ->
+            ok %% already started
+    end.
+            
